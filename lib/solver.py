57bd73b61faf69551ac57c9ecd15989698ebafda
Andrei Chertkov <andrei.chertkov@skolkovotech.ru>
2019-08-11 13:35:23 +0300

    Some fixes

--- lib/solver.py
+++ lib/solver.py
@@ -15,13 +15,12 @@ class Solver(object):
         self.with_tt = with_tt
 
     def set_grid_t(self, t_poi, t_min=0., t_max=1.):
+        self.t_poi = t_poi
         self.t_min = t_min
         self.t_max = t_max
-        self.t_poi = t_poi
 
         self.m = t_poi
         self.h = (t_max - t_min) / (t_poi - 1)
-
         self.T = np.linspace(t_min, t_max, t_poi)
 
     def set_grid_x(self, n, l=[-3., 3.]):
@@ -59,7 +58,7 @@ class Solver(object):
         X0 = X - self.h * f
         I1 = X0 < -3. # TODO! replace by real x_lim
         I2 = X0 > +3. # TODO! replace by real x_lim
-        
+
         r0 = self.IT0.calc(X0)
         r0[I1.reshape(-1)] = 0.
         r0[I2.reshape(-1)] = 0.
@@ -78,3 +77,36 @@ class Solver(object):
             # if i>10: break
 
         self.IT2 = self.IT.copy()
+
+    def info(self):
+        print('Info')
+        # print('Info from Intertrain')
+        # self.IT.info()
+
+    def plot(self):
+        if self.d == 1:
+            return self.plot_1d()
+
+        raise NotImplementedError('Dim %d is not supported for plot'%self.d)
+
+    def plot_1d(self):
+        fig = plt.figure(figsize=(6, 6))
+        gs = mpl.gridspec.GridSpec(
+            ncols=1, nrows=1, left=0.01, right=0.99, top=0.99, bottom=0.01,
+            wspace=0.4, hspace=0.3, width_ratios=[1], height_ratios=[1]
+        )
+
+        Xg = self.IT.grid()
+        x0 = Xg.reshape(-1)
+        r1 = self.IT1.calc(Xg).reshape(-1)
+        r2 = self.IT2.calc(Xg).reshape(-1)
+        ax = fig.add_subplot(gs[0, 0])
+        ax.plot(x0, r1, label='Initial')
+        ax.plot(x0, r2, label='Final (calc)')
+        ax.semilogy()
+        ax.set_title('Probability density function')
+        ax.set_xlabel('x')
+        ax.set_ylabel('r')
+        ax.legend(loc='best')
+
+        plt.show()
