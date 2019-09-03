917b96f243c0e0559da684418bf158a07052ba92
Andrei Chertkov <andrei.chertkov@skolkovotech.ru>
2019-09-03 14:39:38 +0300

    Api for order selection in solver

--- lib/solver.py
+++ lib/solver.py
@@ -3,13 +3,13 @@ import time
 import numpy as np
 from numpy import kron as kron
 from scipy.linalg import expm as expm
-from scipy.integrate import solve_ivp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import animation, rc
 from tqdm import tqdm
 
 from config import config
+from utils import rk4, eul
 from intertrain import Intertrain
 
 class Solver(object):
@@ -261,19 +261,57 @@ class Solver(object):
         type: ndarray [number of points] of float
         '''
 
-        f0 = self.func_f0(X, self.t)
-        X0 = X - self.h * f0
+        def step_x(X):
+            if self.ord == 1:
+                X0 = eul(self.func_f0, X, self.t, self.t - self.h, t_poi=2)
+            else:
+                X0 = rk4(self.func_f0, X, self.t, self.t - self.h, t_poi=2)
 
-        r0 = self.IT0.calc(X0)
+            return X0
 
-        r1 = self.Z @ r0
+        def step_i(X):
+            r0 = self.IT0.calc(X)
 
-        f1 = self.func_f1(X0, self.t - self.h)
-        r2 = (1. - self.h * np.trace(f1)) * r1
+            return r0
 
-        r = r2
+        def step_w(X, w0):
+            if self.ord == 1:
+                f1 = self.func_f1(X, self.t - self.h)
+                w = (1. - self.h * np.trace(f1)) * w0
+            else:
+                def func(v, t):
+                    x = v[:-1, :]
+                    r = v[-1, :]
 
-        return r
+                    f0 = self.func_f0(x, t)
+                    f1 = self.func_f1(x, t)
+
+                    return np.vstack([f0, -np.trace(f1) * r])
+
+                v0 = np.vstack([X, w0])
+                v1 = rk4(func, v0, self.t - self.h, self.t, t_poi=2)
+                w = v1[-1, :]
+
+            return w
+
+        def step_v(X, v0):
+            v = self.Z @ v0
+            return v
+
+        X0 = step_x(X)
+        r0 = step_i(X0)
+        w1 = step_w(X0, r0)
+        v1 = step_v(X0, w1)
+        r1 = v1.copy()
+
+        if self.ord == 1:
+            return r1
+
+        v1 = step_v(X0, r0)
+        w1 = step_w(X0, v1)
+        r2 = w1.copy()
+
+        return (r1 + r2) / 2.
 
     def info(self):
         '''
