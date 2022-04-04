04d4439516dc35f21ea55db4d3335ce2c9c2fd9c
AndreiChertkov <a.chertkov@skoltech.ru>
2022-04-04 19:39:55 +0300

    Service files

---
+++ fpcross/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1'
+
+
+from .equation import *
+from .fpcross import FPCross
---
+++ fpcross/equation/__init__.py
@@ -0,0 +1,4 @@
+from .equation import Equation
+from .equation_dif import EquationDif
+from .equation_dum import EquationDum
+from .equation_oup import EquationOUP
