d19c74f59deb8aaf530353d4aa34a3ace287e719
AndreiChertkov <andre.chertkov@gmail.com>
2022-10-06 14:36:38 +0300

    Update imports

--- fpcross/__init__.py
+++ fpcross/__init__.py
@@ -4,3 +4,4 @@ __version__ = '0.5.1'
 from .equation import Equation
 from .equation_demo import *
 from .fpcross import FPCross
+from .fpcross_oup_diag import FPCrossOUPDiag
--- fpcross/equation_demo/__init__.py
+++ fpcross/equation_demo/__init__.py
@@ -1,3 +1,4 @@
 from .equation_dif import EquationDif
 from .equation_dum import EquationDum
 from .equation_oup import EquationOUP
+from .equation_oup_univ import EquationOUPUniv
