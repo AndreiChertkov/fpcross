7c6b8b0aad2b5a8ba2b7b6ce4ae838ac0472dc47
AndreiChertkov <andre.chertkov@gmail.com>
2023-04-27 21:48:59 +0300

    Remove unused scripts

--- fpcross/__init__.py
+++ fpcross/__init__.py
@@ -4,4 +4,3 @@ __version__ = '0.5.3'
 from .equation import Equation
 from .equation_demo import *
 from .fpcross import FPCross
-from .fpcross_oup_diag import FPCrossOUPDiag
--- fpcross/equation_demo/__init__.py
+++ fpcross/equation_demo/__init__.py
@@ -1,4 +1,3 @@
 from .equation_dif import EquationDif
 from .equation_dum import EquationDum
 from .equation_oup import EquationOUP
-from .equation_oup_univ import EquationOUPUniv
